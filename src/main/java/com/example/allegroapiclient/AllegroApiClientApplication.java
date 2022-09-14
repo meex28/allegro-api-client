30174501dd6444360f588cbd09b9398b25695f63
meex28 <71528905+meex28@users.noreply.github.com>
2022-09-14 13:06:34 +0200

    first commit

---
+++ src/main/java/com/example/allegroapiclient/AllegroApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.allegroapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllegroApiClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllegroApiClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/allegroapiclient/AllegroApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.allegroapiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllegroApiClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
