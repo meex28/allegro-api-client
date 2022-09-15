97b30b8e86e7efa8265dd0fd972da8434e0e47c5
meex28 <71528905+meex28@users.noreply.github.com>
2022-09-15 22:22:33 +0200

    add "token for application" generation

--- src/main/java/com/example/allegroapiclient/AllegroAuthApiService.java
+++ src/main/java/com/example/allegroapiclient/AllegroAuthApiService.java
@@ -1,23 +1,32 @@
 package com.example.allegroapiclient;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriBuilder;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class AllegroAuthApiService {
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
+
     // Endpoints for Allegro API
     private final String baseAllegroUrl = "allegro.pl";
     private final String baseAllegroSandboxUrl = "allegro.pl.allegrosandbox.pl";
+    private final String generateAccessTokenUrl = "/auth/oauth/token";
     private final String signInToAuthorizeURL = "/auth/oauth/authorize";
 
     //TODO: make redirect uri in properties
-    private final String redirectUri = "http://localhost:8080/api/apps/code";
-    public AllegroAuthApiService(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
+    private final String redirectUri = "http://localhost:8080/";
+
+    public AllegroAuthApiService() {
+        this.webClient = WebClient.builder().build();
     }
 
     private UriComponentsBuilder getBasicUriComponentsBuilder(boolean isSandbox){
@@ -29,12 +38,19 @@ public class AllegroAuthApiService {
 
     public String generateAccessCode(String clientId, String clientSecret, boolean isSandbox){
         String url = getBasicUriComponentsBuilder(isSandbox)
-                .path(signInToAuthorizeURL)
-                .queryParam("client_id", clientId)
-                .queryParam("client_secret", clientSecret)
-                .queryParam("redirect_uri", redirectUri)
+                .path(generateAccessTokenUrl)
                 .build().toUriString();
 
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.put("grant_type", Collections.singletonList("client_credentials"));
 
+        WebClient.RequestHeadersSpec<?> responseSpec = webClient.post()
+                .uri(url)
+                .headers(headers -> headers.setBasicAuth(clientId, clientSecret))
+                .headers(headers -> headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .body(BodyInserters.fromFormData(body));
+
+        return responseSpec.retrieve().bodyToMono(String.class)
+                .block();
     }
 }
--- src/test/java/com/example/allegroapiclient/AllegroApiClientApplicationTests.java
+++ src/test/java/com/example/allegroapiclient/AllegroApiClientApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.allegroapiclient;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.*;
 
 @SpringBootTest
 class AllegroApiClientApplicationTests {
+    @Autowired
+    AllegroAuthApiService allegroAuthApiService;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void generateAccessToken(){
+        String clientId = "a5f357c6035d440f84d41d5a762101f9";
+        String clientSecret = "hMtRrbiQ4YchZ6MJNoFaRQwrm7PGO7R1LBFQj9K9h4zKAgeGJcmJguhpvC2d5Nqh";
+        String token = allegroAuthApiService.generateAccessCode(clientId, clientSecret, true);
+        System.out.println(token);
+    }
 }
