fc61f713042971fe5caefd3cad2eb634f7409b1d
meex28 <71528905+meex28@users.noreply.github.com>
2022-09-17 12:01:10 +0200

    add service for apps

---
+++ src/main/java/com/example/allegroapiclient/AllegroAppService.java
@@ -0,0 +1,10 @@
+package com.example.allegroapiclient;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AllegroAppService {
+    public void addTokenForUserToApp(String code, String endpoint){
+
+    }
+}
