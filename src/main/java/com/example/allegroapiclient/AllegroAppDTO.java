319ba0f8461494051cdb1f499cd4dadcde758ad6
meex28 <71528905+meex28@users.noreply.github.com>
2022-09-26 13:53:14 +0200

    change jackson field name

--- src/main/java/com/example/allegroapiclient/AllegroAppDTO.java
+++ src/main/java/com/example/allegroapiclient/AllegroAppDTO.java
@@ -1,12 +1,18 @@
 package com.example.allegroapiclient;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class AllegroAppDTO {
     private String clientId;
+
     private String clientSecret;
+
+    @JsonProperty(value="isSandbox")
     private boolean isSandbox;
+
     private String username;
+
     private String endpoint;
 }
