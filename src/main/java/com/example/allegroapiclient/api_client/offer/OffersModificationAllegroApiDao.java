6fe6d6fc4785ef6ccedfb82b865ea94b2946b0de
meex28 <71528905+meex28@users.noreply.github.com>
2022-10-12 10:40:50 +0200

    add all batch offer modification endpoints

--- src/main/java/com/example/allegroapiclient/api_client/offer/OffersModificationAllegroApiDao.java
+++ src/main/java/com/example/allegroapiclient/api_client/offer/OffersModificationAllegroApiDao.java
@@ -34,6 +34,30 @@ public class OffersModificationAllegroApiDao {
             return json.getString("id");
     }
 
+    private String batchModification(String uri, JSONObject modifications, List<String> offersIds, Token token){
+        List<JSONObject> offers = offersIds.stream()
+                .map(offer -> new JSONObject().put("id", offer))
+                .collect(Collectors.toList());
+        JSONArray offerCriteria = new JSONArray()
+                .put(new JSONObject()
+                        .put("offers", new JSONArray(offers))
+                        .put("type", "CONTAINS_OFFERS"));
+
+        JSONObject body = new JSONObject()
+                .put("modification", modifications)
+                .put("offerCriteria", offerCriteria);
+
+        String responseBody = webClient.put()
+                .uri(uri)
+                .headers(headers -> headers.setBearerAuth(token.token()))
+                .body(BodyInserters.fromValue(body.toString()))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return commandIdOrException(responseBody);
+    }
+
     public String modifyBuyNowPrice(String commandId, String offerId, String amount, String currency, Token token){
         JSONObject body = new JSONObject()
                 .put("id", commandId)
@@ -60,30 +84,175 @@ public class OffersModificationAllegroApiDao {
     }
 
     public String modifyOffers(String commandId, JSONObject modifications, List<String> offersIds, Token token){
-        List<JSONObject> offers = offersIds.stream()
-                .map(offer -> new JSONObject().put("id", offer))
-                .collect(Collectors.toList());
-        JSONArray offerCriteria = new JSONArray()
-                .put(new JSONObject()
-                        .put("offers", new JSONArray(offers))
-                        .put("type", "CONTAINS_OFFERS"));
-        JSONObject body = new JSONObject()
-                .put("modification", modifications)
-                .put("offerCriteria", offerCriteria);
-
         String uri = APIUtils.getBasicUriComponentsBuilder(token.isSandbox())
                 .pathSegment("/sale/offer-modification-commands")
                 .pathSegment(commandId)
                 .build().toUriString();
 
-        String responseBody = webClient.put()
+        return batchModification(uri, modifications, offersIds, token);
+    }
+
+    // Methods to change offers price
+    private enum OfferPriceModificationTypes{
+        FIXED_PRICE, INCREASE_PRICE, DECREASE_PRICE, INCREASE_PERCENTAGE, DECREASE_PERCENTAGE
+    }
+
+    private String modifyOffersPrice(String commandId, JSONObject modifications, List<String> offersIds, Token token){
+        String uri = APIUtils.getBasicUriComponentsBuilder(token.isSandbox())
+                .pathSegment("/sale/offer-modification-commands")
+                .pathSegment(commandId)
+                .build().toUriString();
+
+        return batchModification(uri, modifications, offersIds, token);
+    }
+
+    public String modifyOffersFixedPrice(String commandId, String amount, String currency, List<String> offersIds,
+                                          Token token){
+        JSONObject modification = new JSONObject()
+                .put("type", OfferPriceModificationTypes.FIXED_PRICE)
+                .put("price", new JSONObject()
+                        .put("amount", amount)
+                        .put("currency", currency));
+
+        return modifyOffersPrice(commandId, modification, offersIds, token);
+    }
+
+    public String increaseOffersPrice(String commandId, String amount, String currency, List<String> offersIds,
+                                      Token token){
+        JSONObject modification = new JSONObject()
+                .put("type", OfferPriceModificationTypes.INCREASE_PRICE)
+                .put("price", new JSONObject()
+                        .put("amount", amount)
+                        .put("currency", currency));
+
+        return modifyOffersPrice(commandId, modification, offersIds, token);
+    }
+
+    public String decreaseOffersPrice(String commandId, String amount, String currency, List<String> offersIds,
+                                      Token token){
+        JSONObject modification = new JSONObject()
+                .put("type", OfferPriceModificationTypes.DECREASE_PRICE)
+                .put("price", new JSONObject()
+                        .put("amount", amount)
+                        .put("currency", currency));
+
+        return modifyOffersPrice(commandId, modification, offersIds, token);
+    }
+
+    public String increasePercentageOffersPrice(String commandId, String percentage, List<String> offersIds,
+                                                Token token){
+        JSONObject modification = new JSONObject()
+                .put("type", OfferPriceModificationTypes.INCREASE_PERCENTAGE)
+                .put("percentage", percentage);
+
+        return modifyOffersPrice(commandId, modification, offersIds, token);
+    }
+
+    public String decreasePercentageOffersPrice(String commandId, String percentage, List<String> offersIds,
+                                                Token token){
+        JSONObject modification = new JSONObject()
+                .put("type", OfferPriceModificationTypes.DECREASE_PRICE)
+                .put("percentage", percentage);
+
+        return modifyOffersPrice(commandId, modification, offersIds, token);
+    }
+
+    // Methods to change offers quantity
+    private enum OfferQuantityModificationTypes{
+        FIXED, GAIN
+    }
+
+    private String modifyOffersQuantity(String commandId, JSONObject modifications, List<String> offersIds,
+                                        Token token){
+        String uri = APIUtils.getBasicUriComponentsBuilder(token.isSandbox())
+                .pathSegment("/sale/offer-quantity-change-commands")
+                .pathSegment(commandId)
+                .build().toUriString();
+
+        return batchModification(uri, modifications, offersIds, token);
+    }
+
+    public String modifyOffersFixedQuantity(String commandId, String value, List<String> offersIds, Token token){
+        JSONObject modification = new JSONObject()
+                .put("changeType", OfferQuantityModificationTypes.FIXED)
+                .put("value", value);
+
+        return modifyOffersQuantity(commandId, modification, offersIds, token);
+    }
+
+    public String modifyOffersGainQuantity(String commandId, String value, List<String> offersIds, Token token){
+        JSONObject modification = new JSONObject()
+                .put("changeType", OfferQuantityModificationTypes.GAIN)
+                .put("value", value);
+
+        return modifyOffersQuantity(commandId, modification, offersIds, token);
+    }
+
+    // Methods working with command summaries and reports
+    private JSONObject commandSummaryOrReport(String uri, String token){
+        String responseBody = webClient.get()
                 .uri(uri)
-                .headers(headers -> headers.setBearerAuth(token.token()))
-                .body(BodyInserters.fromValue(body.toString()))
+                .headers(headers -> headers.setBearerAuth(token))
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
+        return new JSONObject(responseBody);
+    }
 
-        return commandIdOrException(responseBody);
+    public JSONObject modificationCommandSummary(String commandId, Token token){
+        String uri = APIUtils.getBasicUriComponentsBuilder(token.isSandbox())
+                .pathSegment("/sale/offer-modification-commands")
+                .pathSegment(commandId)
+                .build().toUriString();
+
+        return commandSummaryOrReport(uri, token.token());
+    }
+
+    public JSONObject changePriceCommandSummary(String commandId, Token token){
+        String uri = APIUtils.getBasicUriComponentsBuilder(token.isSandbox())
+                .pathSegment("/sale/offer-price-change-commands")
+                .pathSegment(commandId)
+                .build().toUriString();
+
+        return commandSummaryOrReport(uri, token.token());
+    }
+
+    public JSONObject changeQuantityCommandSummary(String commandId, Token token){
+        String uri = APIUtils.getBasicUriComponentsBuilder(token.isSandbox())
+                .pathSegment("/sale/offer-quantity-change-commands")
+                .pathSegment(commandId)
+                .build().toUriString();
+
+        return commandSummaryOrReport(uri, token.token());
+    }
+
+    public JSONObject modificationCommandDetailedReport(String commandId, Token token){
+        String uri = APIUtils.getBasicUriComponentsBuilder(token.isSandbox())
+                .pathSegment("/sale/offer-modification-commands")
+                .pathSegment(commandId)
+                .pathSegment("/tasks")
+                .build().toUriString();
+
+        return commandSummaryOrReport(uri, token.token());
+    }
+
+    public JSONObject changePriceDetailedReport(String commandId, Token token){
+        String uri = APIUtils.getBasicUriComponentsBuilder(token.isSandbox())
+                .pathSegment("/sale/offer-price-change-commands")
+                .pathSegment(commandId)
+                .pathSegment("/tasks")
+                .build().toUriString();
+
+        return commandSummaryOrReport(uri, token.token());
+    }
+
+    public JSONObject changeQuantityDetailedReport(String commandId, Token token){
+        String uri = APIUtils.getBasicUriComponentsBuilder(token.isSandbox())
+                .pathSegment("/sale/offer-quantity-change-commands")
+                .pathSegment(commandId)
+                .pathSegment("/tasks")
+                .build().toUriString();
+
+        return commandSummaryOrReport(uri, token.token());
     }
 }
