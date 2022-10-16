ce8ab2e0d723db5d9d2409eb7e71ae1c66a246a9
meex28 <71528905+meex28@users.noreply.github.com>
2022-10-16 19:11:39 +0200

    add tokens to debug

---
+++ src/test/java/com/example/allegroapiclient/TestTokens.java
@@ -0,0 +1,8 @@
+package com.example.allegroapiclient;
+
+public class TestTokens {
+    public final static String clientId = "636ca94c10f44327b36b7f2d55635b72";
+    public final static String clientSecret = "YBJd3XO2nnUwfvFdixdLddz4UCpRhOya4TiOcDXHYjEFNYUwZJLvzM39vWOZPIt4";
+    public final static String endpoint = "test-endpoint";
+    public final static String username = "meex-sandbox";
+}
